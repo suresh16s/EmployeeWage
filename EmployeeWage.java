7e819b8fd782e52006a6d108b5f2681f79758a35
suresh16s <sureshthite48@gmail.com>
2020-07-23 19:24:27 +0530

    UC_1Attendance

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,5 +1,13 @@
-class EmployeeWage{
-public static void main(String []args){
-System.out,println("Welcome to Employee Wage Program:");
+public class EmployeeWage
+{
+
+public static void main(String args[]){
+		final int fullTime=1;
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if ( empCheck == fullTime )
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Absent");
 }
+
 }
